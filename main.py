24c4604698128cb2950a0643ef3f7ba3b374e1a1
Oshri Magnezi <oshrimagen0@gmail.com>
2026-09-14 18:15:40 +0300

    init: second_largest function

---
+++ main.py
@@ -0,0 +1,12 @@
+class Rectangle:
+
+    def __init__(self, width: float, height: float) -> None:
+        self.width = width
+        self.height = height
+
+    def area(self) -> float:
+        return self.width * self.height
+
+if __name__ == '__main__':
+    rect = Rectangle(4, 5)
+    print(rect.area())
